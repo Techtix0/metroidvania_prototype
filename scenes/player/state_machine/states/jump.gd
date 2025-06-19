f75979d65701a0beb53fe4959b550646ee32d580
Techtix0 <techtix0@proton.me>
2025-06-19 22:39:37 +0200

    Add: fall and jump states

---
+++ scenes/player/state_machine/states/jump.gd
@@ -0,0 +1,32 @@
+extends State
+
+@export var fall_state: State
+@export var move_state: State
+
+@export var jump_force: float = 300.0
+
+func enter() -> void:
+	super()
+	animation_name = "jump"
+	parent.velocity.y = -jump_force
+
+func process_input(event: InputEvent) -> State:
+	return null
+
+func process_physics(delta: float) -> State:
+	parent.velocity.y += gravity * delta
+	
+	if !parent.is_on_floor():
+		return fall_state
+	
+	var movement = Input.get_axis("move_left", "move_right") * move_speed
+
+	if movement != 0:
+		parent.animations.flip_h = movement < 0
+	parent.velocity.x = movement
+	parent.move_and_slide()
+	
+	if parent.is_on_floor() and movement != 0:
+		return move_state
+	return null
+	

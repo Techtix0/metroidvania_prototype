77916dc58374459271310a177d16690b65248e0d
Techtix0 <techtix0@proton.me>
2025-06-20 07:17:44 +0200

    Add: weapon display in inventory

--- src/scenes/hud/pause_menu/menu_overlay.gd
+++ src/scenes/hud/pause_menu/menu_overlay.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+func _ready() -> void:
+	%SwordDisplay.visible = PlayerManager.inventory["Sword"] == true
+
 func _unhandled_input(_vent: InputEvent) -> void:
 	if Input.is_action_just_pressed("toggle_menu") and get_tree().paused and get_parent().menu_opened:
 		get_tree().paused = false

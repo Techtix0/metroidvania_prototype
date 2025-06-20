9554bae4c83f6397832106e672b61eff602b3565
Techtix0 <techtix0@proton.me>
2025-06-20 05:47:08 +0200

    Add: menu screen

--- src/scenes/player/player.gd
+++ src/scenes/player/player.gd
@@ -4,18 +4,35 @@ extends CharacterBody2D
 @onready var animations: AnimatedSprite2D = $Animations
 @onready var state_machine: Node = $StateMachine
 @onready var move_component: Node = $MoveComponent
+@onready var get_menu := preload("res://src/scenes/hud/menu_overlay.tscn")
 
 @export var move_speed: float
+
+var menu_opened: bool = false
 
 func _ready() -> void:
 	add_to_group("Player")
 	state_machine.init(self, animations, move_component, move_speed)
 
 func _unhandled_input(event: InputEvent) -> void:
-	state_machine.process_input(event)
+	if Input.is_action_just_pressed("toggle_menu"):
+		toggle_menu()
+	else:
+		state_machine.process_input(event)
 
 func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta)
+	
 
 func _process(delta: float) -> void:
 	state_machine.process_frame(delta)
+
+func toggle_menu():
+	if !menu_opened:
+		add_child(get_menu.instantiate())
+		menu_opened = true
+	else:
+		var menu_overlay = get_node("MenuOverlay")
+		if menu_overlay:
+			menu_overlay.queue_free()
+		menu_opened = false

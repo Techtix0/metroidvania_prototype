1f012907939ec42d3a877be888d54399365c25b5
Techtix0 <techtix0@proton.me>
2025-06-22 03:07:58 +0200

    Add: controller support and deligated pause menu to gameManager

--- src/managers/game_manager.gd
+++ src/managers/game_manager.gd
@@ -1,8 +1,30 @@
 extends Node
 
+@onready var get_menu := preload("res://src/scenes/hud/pause_menu/menu_overlay.tscn")
+var menu_opened: bool
+
 func _ready() -> void:
+	menu_opened = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("toggle_menu"):
+		toggle_menu()
 	
 # TODO: Call this function from the main menu
 func start_game() -> void:
 	SceneManager.transition_to_scene("Level01")
+	
+func toggle_menu():
+	if !menu_opened:
+		add_child(get_menu.instantiate())
+		menu_opened = true
+		get_tree().paused = true
+	else:
+		# Unpausing the game is done in the menu overlay script itself
+		# since the player entity is paused alongside the rest of the game
+		var menu_overlay
+		for child in get_children():
+			if child is MenuOverlay:
+				child.queue_free()
+		menu_opened = false
--- src/scenes/player/player.gd
+++ src/scenes/player/player.gd
@@ -4,37 +4,16 @@ extends CharacterBody2D
 @onready var animations: AnimatedSprite2D = $Animations
 @onready var state_machine: Node = $StateMachine
 @onready var move_component: Node = %PlayerMoveComponent
-@onready var get_menu := preload("res://src/scenes/hud/pause_menu/menu_overlay.tscn")
-
-var menu_opened: bool = false
 
 func _ready() -> void:
 	add_to_group("Player")
 	state_machine.init(self, animations, move_component, PlayerManager.move_speed)
 
-
 func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("toggle_menu"):
-		toggle_menu()
-	else:
-		state_machine.process_input(event)
+	state_machine.process_input(event)
 
 func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta)
 	
-
 func _process(delta: float) -> void:
 	state_machine.process_frame(delta)
-
-func toggle_menu():
-	if !menu_opened:
-		add_child(get_menu.instantiate())
-		menu_opened = true
-		get_tree().paused = true
-	else:
-		# Unpausing the game is done in the menu overlay script itself
-		# since the player entity is paused alongside the rest of the game
-		var menu_overlay = get_node("MenuOverlay")
-		if menu_overlay:
-			menu_overlay.queue_free()
-		menu_opened = false

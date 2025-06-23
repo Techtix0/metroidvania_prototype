f4ac65d1389427e6bbc2ff990bc1585aa8ed608c
Techtix0 <techtix0@proton.me>
2025-06-23 08:15:10 +0200

    Add: comments

--- src/scenes/player/player_manager.gd
+++ src/scenes/player/player_manager.gd
@@ -3,25 +3,33 @@ extends Node
 signal obtain_sword
 signal took_damage
 
+# Player object
 var player: Player
 
+# Player inventory
 var inventory: Dictionary = {"Sword" : false}
 
-var player_position: Vector2
-var came_through_door: bool = false
-
+# Player health
 var max_health: int = 100
 var current_health: int = max_health
 
+# Where to spawn player on next scene load
+var player_position: Vector2
+var came_through_door: bool = false
+
 func _ready() -> void:
+	# Sets initial player position
 	player_position = Vector2(32, 280)
 
+# Refresh the link to the player object
 func link_player(player: Player) -> void:
 	self.player = player
 
+# Emit signal when sword upgrade is picked up by the player
 func announce_sword_upgrade() -> void:
 	emit_signal("obtain_sword")
 
+# Modify health based on damage taken
 func take_damage(amount: int) -> void:
 	if current_health > 0:
 		current_health -= amount

8f949ea311511b8d8b80a384b0eec430b62c4425
Techtix0 <techtix0@proton.me>
2025-06-23 09:07:05 +0200

    Add: dynamic animation length for dash based on dash length

--- src/state_machine/states/dash.gd
+++ src/state_machine/states/dash.gd
@@ -5,11 +5,17 @@ extends State
 
 var dash_timer: float
 var movement: float
+var total_frames: int
 
 func enter() -> void:
-	animation_name = "idle"
+	animation_name = "dash"
+	# Stopping animation since the frames will be manually set during the dash based on the dash_time
+	parent.animations.stop()
+
 	parent.velocity = Vector2(0, 0)
 	dash_timer = move_component.dash_time
+
+	total_frames = parent.animations.sprite_frames.get_frame_count(animation_name)
 
 	movement = ceil(move_component.get_movement_direction()) * move_component.dash_speed 
 	if movement == 0:
@@ -24,6 +30,9 @@ func exit() -> void:
 	pass
 
 func process_physics(delta: float) -> State:
+	# Setting the frame of the animation based on the progress in the dash timer
+	parent.animations.frame = round(total_frames / (move_component.dash_time / dash_timer))
+
 	if dash_timer > 0:
 		dash_timer -= delta
 		parent.animations.flip_h = movement < 0
